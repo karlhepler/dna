2fbdbb03c19e7978dd25c0f822f0da2a2664678a
Karl Hepler <khepler@brandmovers.com>
2016-02-20 13:08:45 -0500

    Started thinking about the dna class

---
+++ source/non-appable/src/NonAppable/DNA.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NonAppable;
+
+class DNA
+{
+    public function define($word)
+    {
+		// Check to see if it's in the list
+		if ( $this->isDefined($word) ) {
+			return $this->repository
+				->where()
+		}
+
+		return $this->webster->
+    }
+}
